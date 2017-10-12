ba3161a8c3983f2c258df25b5279e88e44bc4ade
Liss Tarnell <ft@le-Fay.ORG>
2017-10-12 10:09:44 +0100

    travis-build.sh: push testing image on every build

--- travis-build.sh
+++ travis-build.sh
@@ -14,14 +14,22 @@ printf '####################################################################\n'
 printf '>>> Building Docker image.\n\n'
 docker build --pull -t torchbox/kdtool:$COMMIT .
 
-# If this is a release, push the Docker image to Docker Hub.
-if [ "$TRAVIS_PULL_REQUEST" = "false" -a -n "$TRAVIS_TAG" ]; then
+if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
 	printf '####################################################################\n'
-	printf '>>> Creating release.\n\n'
-
+	printf '>>> Pushing test release.\n\n'
+	# Push the latest build to the 'testing' tag.
 	docker login -u $DOCKER_USER -p $DOCKER_PASSWORD
-	docker tag torchbox/kdtool:$COMMIT torchbox/kdtool:$TRAVIS_TAG
-	docker push torchbox/kdtool:$TRAVIS_TAG
-	docker tag torchbox/kdtool:$COMMIT torchbox/kdtool:latest
-	docker push torchbox/kdtool:latest
+	docker tag torchbox/kdtool:$COMMIT torchbox/kdtool:testing
+	docker push torchbox/kdtool:testing
+
+	# If this is a release, push the Docker image to Docker Hub.
+	if [ -a -n "$TRAVIS_TAG" ]; then
+		printf '####################################################################\n'
+		printf '>>> Creating release.\n\n'
+
+		docker tag torchbox/kdtool:$COMMIT torchbox/kdtool:$TRAVIS_TAG
+		docker push torchbox/kdtool:$TRAVIS_TAG
+		docker tag torchbox/kdtool:$COMMIT torchbox/kdtool:latest
+		docker push torchbox/kdtool:latest
+	fi
 fi
